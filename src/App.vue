af3f5b2d14d3ac9ccd64166ca43fdbe1df3994ae
Darragh Enright <darragh@bodhala.com>
2022-09-21 00:34:26 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,47 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  computed: {
+    answerLang: ({ lang }) => (lang === "fr" ? "en" : "fr"),
+  },
   created() {
     fetch("flashcards.json")
       .then((response) => response.json())
       .then((cards) => (this.cards = cards))
-      .then(() => this.pickQuestion());
+      .then(() => this.pickCard());
   },
   data() {
     return {
       answer: "",
       cards: [],
-      lang: "fr",
+      lang: "en",
       question: null,
+      score: {
+        yes: 0,
+        no: 0,
+      },
     };
   },
   methods: {
-    pickQuestion() {
+    checkAnswer() {
+      if (this.answer == this.question[this.answerLang]) {
+        this.score.yes += 1;
+        this.pickCard();
+      }
+    },
+    showAnswer() {
+      this.answer = this.question[this.answerLang];
+      this.score.no += 1;
+      setTimeout(() => this.pickCard(), 1_500);
+    },
+    pickCard() {
       this.answer = "";
       this.question = this.cards[Math.floor(Math.random() * this.cards.length)];
     },
   },
   watch: {
-    answer(answer) {
-      let answerLang = this.lang === "fr" ? "en" : "fr";
-
-      if (answer == this.question[answerLang]) {
-        this.pickQuestion();
-      }
-    },
     lang() {
-      this.pickQuestion();
+      this.pickCard();
       this.$refs["answer"].focus();
     },
   },
@@ -41,15 +52,29 @@ export default defineComponent({
 <template>
   <main class="grid h-screen place-items-center text-center">
     <div v-if="question">
-      <div class="text-4xl mb-8">{{ question[lang] }}</div>
-      <input
-        autofocus
-        class="border p-2"
-        type="text"
-        v-model.trim="answer"
-        ref="answer"
-      />
-      <div class="text-center mt-8">
+      <div class="text-4xl mb-6">{{ question[lang] }}</div>
+      <div class="mb-3">
+        <span class="text-red-500 text-lg inline-block mr-3">
+          {{ score.no }}
+        </span>
+        <input
+          autofocus
+          class="border p-2 rounded"
+          ref="answer"
+          tabindex="0"
+          type="text"
+          v-model.trim="answer"
+          v-on:keyup.esc="showAnswer"
+          v-on:keyup.enter="checkAnswer"
+        />
+        <span class="text-green-500 text-lg inline-block ml-3">
+          {{ score.yes }}
+        </span>
+      </div>
+      <p class="text-xs text-gray-400 font-light mb-6">
+        Type answer and press ENTER, or ESC to skip.
+      </p>
+      <div class="text-center">
         <label class="mr-2" for="lang-en">
           <input id="lang-en" type="radio" v-model="lang" value="en" /> EN
         </label>
